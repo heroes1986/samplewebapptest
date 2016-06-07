ea1e01a24c8eeff33b41e1902dac330c301b7f33
kokeng <kokeng86@gmail.com>
2016-06-07 18:39:14 +0800

    first implementation

---
+++ SampleWebAutomationTest/DriverFactory.cs
@@ -0,0 +1,57 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.PhantomJS;
+
+namespace SampleWebAutomationTest
+{
+    public enum DriverType
+    {
+        PhantomJs = 0,
+        Chrome
+    }
+
+    public interface IDriverFactory
+    {
+        /// <summary>
+        /// Gets the driver.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <returns></returns>
+        IWebDriver GetDriver(DriverType type);
+    }
+    public class DriverFactory : IDriverFactory
+    {
+        private string _directory = @"C:\Project\Others\SampleWebAutomationTest\packages\EXEFiles"; 
+        private static IDriverFactory _instance;
+        public static IDriverFactory Instance
+        {
+            get { return _instance ?? (_instance = new DriverFactory()); }
+        }
+
+        /// <summary>
+        /// Gets the driver.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <returns></returns>
+        public IWebDriver GetDriver(DriverType type)
+        {
+            switch (type)
+            {
+                case DriverType.PhantomJs:
+                {
+                    var service = PhantomJSDriverService.CreateDefaultService();
+                    service.HideCommandPromptWindow = true;
+                    return new PhantomJSDriver(service);
+                }
+                case DriverType.Chrome:
+                {
+                    var service = ChromeDriverService.CreateDefaultService();
+                    service.HideCommandPromptWindow = true;
+                    return new ChromeDriver(service);
+                }
+                default:
+                    return null;
+            }
+        }
+    }
+}
---
+++ SampleWebAutomationTest/LandingPageTests.cs
@@ -0,0 +1,97 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace SampleWebAutomationTest
+{
+    [TestFixture(DriverType.PhantomJs)]
+    //[TestFixture(DriverType.Chrome)]
+    public class LandingPageTests
+    {
+        private IWebDriver _driver;
+        private readonly DriverType _type;
+
+        public LandingPageTests(DriverType type)
+        {
+            _type = type;
+        }
+
+        [TestFixtureSetUp]
+        public void OneTimeSetup()
+        {
+            _driver = DriverFactory.Instance.GetDriver(_type);
+        }
+
+        [TestFixtureTearDown]
+        public void OneTimeTearDown()
+        {
+            _driver.Close();
+            _driver.Dispose();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            _driver.Navigate().GoToUrl("http://test.sampleapp.com/");
+        }
+
+        [TestCase("about")]
+        public void About_Navigation_Found_Return_Ok(string id)
+        {
+            // Arrange
+
+
+            // Act
+            var result = _driver.FindElement(By.Id(id));
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestCase("about")]
+        public void About_Navigation_To_About_Page_Return_Ok(string id)
+        {
+            // Arrange
+            var expectedUrl = "http://test.sampleapp.com/Home/About";
+
+            var element = _driver.FindElement(By.Id(id));
+            var url = element.GetAttribute("href");
+           
+            // Act
+            _driver.Navigate().GoToUrl(url);
+
+            // Assert
+            Assert.IsNotNullOrEmpty(url);
+            Assert.AreEqual(expectedUrl, _driver.Url);
+        }
+
+        [TestCase("contact")]
+        public void Contact_Navigation_Found_Return_Ok(string id)
+        {
+            // Arrange
+
+
+            // Act
+            var result = _driver.FindElement(By.Id(id));
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestCase("contact")]
+        public void Contact_Navigation_To_Contact_Page_Return_Ok(string id)
+        {
+            // Arrange
+            var expectedUrl = "http://test.sampleapp.com/Home/Contact";
+
+            var element = _driver.FindElement(By.Id(id));
+            var url = element.GetAttribute("href");
+
+            // Act
+            _driver.Navigate().GoToUrl(url);
+
+            // Assert
+            Assert.IsNotNullOrEmpty(url);
+            Assert.AreEqual(expectedUrl, _driver.Url);
+        }
+    }
+}
